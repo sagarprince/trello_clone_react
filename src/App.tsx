214f597d676dfaaa4ef853e2cb36ae8b6e62ed82
sagar.pansare <>
2023-05-29 03:58:22 +0530

    commit changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { signal } from "@preact/signals-react";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const count = signal(0);
 
+function App() {
   return (
     <>
       <div>
@@ -18,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => count.value++}>
           count is {count}
         </button>
         <p>
